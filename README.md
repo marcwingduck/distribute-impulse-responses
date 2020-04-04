8a994e55d9524a4a7e6c42c6ccfe5b182ad70306
Marc Lieser <marc.lieser@hs-rm.de>
2020-04-04 17:17:13 +0200

    Skip provided Bank Indices

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # distribute-impulse-responses
+
 Recursively distribute IRs from a given directory to the bank/preset directories of the AMT Pangaea IR Convolution Player CP-100
+
+## Examples
+
+Connect device as USB drive.
+
+### Build directory structure and exit
+
+    python3 distribute_irs.py --build --drive dst
+
+### Copy directory containing IRs to device
+
+    python3 distribute_irs.py --collection src --drive dst
+
+### Copy directory but keep banks 1, 4 and 8 untouched
+
+    python3 distribute_irs.py --collection src --drive dst -skip 1 4 8
--- distribute_irs.py
+++ distribute_irs.py
@@ -21,6 +21,16 @@ def silent_remove(filename):
             raise
 
 
+def create_dirs(drive):
+    for i in range(n_banks):
+        for j in range(n_presets):
+            bank_str = "Bank_" + str(i)
+            preset_str = "Preset_" + str(j)
+            directory = os.path.join(drive, bank_str, preset_str)
+            if not os.path.isdir(directory):
+                os.makedirs(directory)
+
+
 def distribute(collection, drive, skip):
     """Distribute the IR collection to the AMT Pangaea USB drive"""
 
@@ -30,40 +40,42 @@ def distribute(collection, drive, skip):
         for name in sorted(files):
             impulses.append(os.path.join(root, name))
 
-    n_skip = skip * n_banks  # number of presets to skip
-    c = n_skip  # start index (skip first bank by default)
+    c = 0
+    for i in range(n_banks):
+        # skip this bank if desired
+        if i in skip:
+            continue
+        for j in range(n_presets):
+            # more impulses than slots
+            if c == len(impulses):
+                break
 
-    for impulse in impulses:
+            # current impulse
+            impulse = impulses[c]
 
-        # more impulses than slots
-        if c == n_irs - n_skip:
-            break
+            bank_str = "Bank_" + str(i)
+            preset_str = "Preset_" + str(j)
+            directory = os.path.join(drive, bank_str, preset_str)
 
-        bank = c / n_banks  # bank number
-        preset = c % n_presets  # preset number
+            # create if necessary
+            if not os.path.isdir(directory):
+                os.makedirs(directory)
 
-        # generate directory names and join directory path
-        bank_str = "Bank_" + str(bank)
-        preset_str = "Preset_" + str(preset)
-        directory = os.path.join(drive, bank_str, preset_str)
+            # remove IRs located in directory
+            for irname in os.listdir(directory):
+                if irname.endswith('.wav'):
+                    silent_remove(os.path.join(directory, irname))
 
-        # if not os.path.isdir(directory):
-        #    os.makedirs(directory)
+            # copy current impulse
+            print('Copying {} to {}... '.format(impulse, os.path.join(directory, os.path.basename(impulse))), end='')
+            copyfile(impulse, os.path.join(directory, os.path.basename(impulse)))
+            print('done.')
 
-        # remove IRs located in directory
-        for irname in os.listdir(directory):
-            if irname.endswith('.wav'):
-                silent_remove(os.path.join(directory, irname))
-
-        # copy current impulse
-        print 'Copying "%s" to "%s"...' % (impulse, os.path.join(directory, os.path.basename(impulse)))
-        copyfile(impulse, os.path.join(directory, os.path.basename(impulse)))
-
-        # increment
-        c += 1
+            # increment
+            c += 1
 
     # print summary
-    print 'Copied %d/%d IRs.' % (c - n_skip, len(impulses))
+    print('Copied {}/{} IRs.'.format(c, len(impulses)))
 
 
 if __name__ == "__main__":
@@ -73,10 +85,15 @@ if __name__ == "__main__":
     # setup arguments
     parser.add_argument('-c', '--collection', help='Directory with IRs to distribute.', type=str, required=True)
     parser.add_argument('-d', '--drive', help='Path to the USB drive.', type=str, required=True)
-    parser.add_argument('-s', '--skip', help='Number of banks to skip.', nargs='?', const=1, type=int, default=1)
+    parser.add_argument('-s', '--skip', help='List of bank numbers to be skipped.', nargs='+', type=int, default=[])
+    parser.add_argument('-b', '--build', help='Build directory structure and exit.', type=bool, default=False)
 
     # parse arguments
     args = parser.parse_args()
+
+    if args.build:
+        create_dirs(args.drive)
+        exit(0)
 
     # generate pattern
     distribute(args.collection, args.drive, args.skip)
